9c50fe8aab88291f4bb0d5576a41ee2ca9636c6f
Oleg Repin <oleg.repin@iqoption.com>
2018-03-22 18:00:57 +0300

    removed build/ folder from git, add ci and publish package from there

--- build/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Format string to UUID format
- * @param {string} value string of 32 hexadecimal numbers
- * @returns {string} formatted toUUID string
- */
-export declare function toUUID(value: string): string;
